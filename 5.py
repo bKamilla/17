0ef3ba292f4c40a2742fe441fc00e99997a8c3b8
bKamilla <k.batrakova@g.nsu.ru>
2024-06-29 22:22:40 +0700

    1

--- 5.py
+++ 5.py
@@ -1,1 +1,24 @@
-5
+count = 0
+with open("input.txt","r") as input_file:
+    for line in input_file:
+        numbers = list(map(int, line.split()))
+        counts = {}
+        for number in numbers:
+            if number in counts:
+                counts[number] += 1
+            else:
+                count[number] = 1
+        count_values = lst(count.values())
+        count_values.sort()
+        if count_values == [1, 1, 3, 5]:
+            avg = sum(numbers) / 10
+            unique_elements = []
+            for number in numbers:
+                if counts[number] == 1:
+                    unique_elements.append(number)
+            for number in unique_elements:
+                if number in unique_elements:
+                    if number > average:
+                        count += 1
+                        break
+print(count)
